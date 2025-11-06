0d1c7cf601711e9143d5bb52061557d8197e04f8
Sean Shanahan <sean.shanahan@vtgdefense.com>
2025-11-06 18:00:32 +0000

    Updated requirements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//For this Coding Challenge your goal is to build an echo server that implements all of the Echo Protocol as defined in RFC 862.
+//1.    In this step your goal is to build a simple server that will start-up, bind to all the local IP addresses, listen on port 7, and accept a TCP connection. 
+//      To complete this step simply have the server print out a log message to show a connection has been accepted and then have it shutdown. 
+//      Refer to the documentation for you programming language to find out how to write network programs using it.
+//2.    In this step your goal is to extend your server to accept multiple concurrent connections. 
+//      This will require you to keep the main ‘thread’ of execution running and listening for incoming connections as well as spawning a new ‘thread’ 
+//      of execution to handle each client. https://codingchallenges.substack.com/p/coding-challenge-101-echo-server
+
 fn main() {
     println!("Hello, world!");
 }
